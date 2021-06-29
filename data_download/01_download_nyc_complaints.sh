43320b3c68523044f1b1a1d785119115bb8763ef
Nikola Baci <nikolabaci98@gmail.com>
2021-06-29 11:57:30 -0400

    download folder

---
+++ data_download/01_download_nyc_complaints.sh
@@ -0,0 +1,5 @@
+# This script download the data for the NYC police complaints
+# Author: Yasiris Ortiz and Nikola Baci
+# Date Edited: June 22nd, 2021
+
+curl https://raw.githubusercontent.com/new-york-civil-liberties-union/NYPD-Misconduct-Complaint-Database-Updated/main/CCRB%20Complaint%20Database%20Raw%2004.20.2021.csv -o nyc_misconduct_complaint_dataset.csv
